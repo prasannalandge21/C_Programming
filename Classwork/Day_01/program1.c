d974d57d6d68260d38d59750ed4489a4a492b9fd
prasannalandge21 <prasannalandge21@gmail.com>
2025-10-13 00:07:43 +0530

    Add Day_01 classwork programs

---
+++ Classwork/Day_01/program1.c
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////
+//
+// Function Name : Print Jay Shree Krishna
+// Input : Character
+// Ouput : Character
+// Author : Prasanna Shamsundar Landge
+// Date : 09/10/25
+//
+//////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Shree Krishna...\n");
+    return 0;
+}
